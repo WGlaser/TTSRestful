d1f7bbf8d912b23be64a4919ffaad5c7df2fa11b
Wade Glaser <wadeglaser@Wades-Air.attlocal.net>
2020-11-27 18:53:22 -0500

    First Commit

---
+++ src/main/java/com/example/demo/OrderModelAssembler.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@Component
+class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+
+  @Override
+  public EntityModel<Order> toModel(Order order) {
+
+    // Unconditional links to single-item resource and aggregate root
+
+    EntityModel<Order> orderModel = EntityModel.of(order,
+        linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+        linkTo(methodOn(OrderController.class).all()).withRel("orders"));
+
+    // Conditional links based on state of the order
+
+    if (order.getStatus() == Status.IN_PROGRESS) {
+      orderModel.add(linkTo(methodOn(OrderController.class).cancel(order.getId())).withRel("cancel"));
+      orderModel.add(linkTo(methodOn(OrderController.class).complete(order.getId())).withRel("complete"));
+    }
+
+    return orderModel;
+  }
+  
+ 
+}
